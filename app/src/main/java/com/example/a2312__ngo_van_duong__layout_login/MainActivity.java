99b2f8e988a34797f6609f8e85616d1a5ad063af
vincentngo-nvd <ngovanduong.a2@gmail.com>
2024-09-12 20:52:51 +0700

    init project

---
+++ app/src/main/java/com/example/a2312__ngo_van_duong__layout_login/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.a2312__ngo_van_duong__layout_login;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText usernameInput;
+    private EditText passwordInput;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        usernameInput = findViewById(R.id.username_input);
+        passwordInput = findViewById(R.id.password_input);
+        Button loginBtn = findViewById(R.id.login_btn);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String username = usernameInput.getText().toString();
+                String password = passwordInput.getText().toString();
+                Log.i("Test Credentials", "Username : " + username + " and Password : " + password);
+            }
+        });
+    }
+}
